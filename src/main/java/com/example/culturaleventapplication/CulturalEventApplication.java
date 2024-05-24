d2f670e75d8df0b82c5935e28150589f06074312
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-05-24 17:40:26 +0200

    init

---
+++ src/main/java/com/example/culturaleventapplication/CulturalEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.culturaleventapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturalEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/culturaleventapplication/CulturalEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.culturaleventapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
