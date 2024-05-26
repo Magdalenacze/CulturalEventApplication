99b03ba278841faa82013405e76f3b0ec3a72351
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-05-26 19:55:16 +0200

    Story 2 -  annotations Repository and Table
    Added missing @Repository and @Table annotations

--- src/main/java/com/example/culturaleventapplication/culturalevent/entity/CulturalEventEntity.java
+++ src/main/java/com/example/culturaleventapplication/culturalevent/entity/CulturalEventEntity.java
@@ -1,10 +1,7 @@
 package com.example.culturaleventapplication.culturalevent.entity;
 
 import com.example.culturaleventapplication.culturalevent.exception.CulturalEventException;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
+@Table(name = "events")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class CulturalEventEntity {
--- src/main/java/com/example/culturaleventapplication/culturalevent/repository/CulturalEventRepository.java
+++ src/main/java/com/example/culturaleventapplication/culturalevent/repository/CulturalEventRepository.java
@@ -2,6 +2,8 @@ package com.example.culturaleventapplication.culturalevent.repository;
 
 import com.example.culturaleventapplication.culturalevent.entity.CulturalEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CulturalEventRepository extends JpaRepository<CulturalEventEntity, Long> {
 }
