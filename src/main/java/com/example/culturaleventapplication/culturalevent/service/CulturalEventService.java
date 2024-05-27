74e9c45f621bb0d64307f923151c76adaa64c7c8
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-05-27 14:32:10 +0200

    Story 3 - Possibility to display all events
    1. Added functionality to display all events
    2. Integration test added

--- src/main/java/com/example/culturaleventapplication/culturalevent/service/CulturalEventService.java
+++ src/main/java/com/example/culturaleventapplication/culturalevent/service/CulturalEventService.java
@@ -2,7 +2,11 @@ package com.example.culturaleventapplication.culturalevent.service;
 
 import com.example.culturaleventapplication.culturalevent.dto.CulturalEventDto;
 
+import java.util.List;
+
 public interface CulturalEventService {
 
     public void createNewEvent(CulturalEventDto culturalEventDto);
+
+    List<CulturalEventDto> getAllEvents();
 }
