d58361028ab54858d2763d816c81e439f56445db
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-06-09 12:24:55 +0200

    additional tests
    Added missing tests

--- src/test/java/com/example/culturaleventapplication/Notification/NotifyServiceTest.java
+++ src/test/java/com/example/culturaleventapplication/Notification/NotifyServiceTest.java
@@ -6,8 +6,10 @@ import com.example.culturaleventapplication.User.dto.UserDto;
 import com.example.culturaleventapplication.User.mapper.Mappers;
 import com.example.culturaleventapplication.User.repository.RepoUsers;
 import com.example.culturaleventapplication.User.service.UsersService;
+import com.example.culturaleventapplication.culturalevent.dto.CulturalEventDto;
 import com.example.culturaleventapplication.culturalevent.entity.CulturalEventEntity;
 import com.example.culturaleventapplication.culturalevent.repository.CulturalEventRepository;
+import com.example.culturaleventapplication.culturalevent.service.CulturalEventService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,10 @@ class NotifyServiceTest {
 
     @Autowired
     private CulturalEventRepository culturalEventRepository;
+
+    @Autowired
+    private CulturalEventService culturalEventService;
+
 
     @Autowired
     private Mappers mappers;
@@ -60,5 +66,74 @@ class NotifyServiceTest {
         assertThat(notifications.get(0).getUser().getNameUser()).isEqualTo(userDto.getNameUser());
         assertThat(notifications.get(0).getUser().getEmailAddress()).isEqualTo(userDto.getEmailAdres());
         assertThat(notifications.get(0).getEventCity().equals(userDto.getCity()));
+    }
+
+    @Test
+    public void should_create_new_notification_successfully() {
+        //given
+        UserDto userDto = new UserDto(
+                "Jason",
+                "Warsaw",
+                "jason@gmail.com");
+        usersService.addUser(userDto);
+
+        CulturalEventDto culturalEventDto = new CulturalEventDto(
+                "Warsaw",
+                "2026-05-26 12:00:00",
+                "Concert");
+
+        //when
+        culturalEventService.createNewEvent(culturalEventDto);
+
+        //then
+        List<NotifyEntity> all = notifyRepo.findAll();
+        assertThat(all).hasSize(1);
+    }
+
+    @Test
+    public void should_not_allow_to_create_new_notification_for_user_from_city_other_than_city_of_the_event() {
+        UserDto userDto = new UserDto(
+                "Jason",
+                "Warsaw",
+                "jason@gmail.com");
+        usersService.addUser(userDto);
+
+        CulturalEventDto culturalEventDto = new CulturalEventDto(
+                "Katowice",
+                "2026-05-26 12:00:00",
+                "Concert");
+
+        //when
+        culturalEventService.createNewEvent(culturalEventDto);
+
+        //then
+        List<NotifyEntity> all = notifyRepo.findAll();
+        assertThat(all).hasSize(0);
+    }
+
+    @Test
+    public void should_write_to_db_all_data_that_should_be_included_in_the_notification_successfully() {
+        //given
+        UserDto userDto = new UserDto(
+                "Jason",
+                "Warsaw",
+                "jason@gmail.com");
+        usersService.addUser(userDto);
+
+        CulturalEventDto culturalEventDto = new CulturalEventDto(
+                "Warsaw",
+                "2026-05-26 12:00:00",
+                "Concert");
+
+        //when
+        culturalEventService.createNewEvent(culturalEventDto);
+
+        //then
+        List<NotifyEntity> all = notifyRepo.findAll();
+        assertThat(all.get(0).getUser()).isNotNull();
+        assertThat(all.get(0).getEventid()).isNotNull();
+        assertThat(all.get(0).getNameOfEvent()).isNotNull();
+        assertThat(all.get(0).getEventCity()).isNotNull();
+        assertThat(all.get(0).getEventDate()).isNotNull();
     }
 }
